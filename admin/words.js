24e932659e7986857335f1088d4033c6a6622d13
andy200877 <andy200877@arcor.de>
2021-11-21 18:26:49 +0100

    start

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'solax-x1-mini-rs485 adapter settings': {
+        'en': 'Adapter settings for solax-x1-mini-rs485',
+        'de': 'Adaptereinstellungen für solax-x1-mini-rs485',
+        'ru': 'Настройки адаптера для solax-x1-mini-rs485',
+        'pt': 'Configurações do adaptador para solax-x1-mini-rs485',
+        'nl': 'Adapterinstellingen voor solax-x1-mini-rs485',
+        'fr': "Paramètres d'adaptateur pour solax-x1-mini-rs485",
+        'it': "Impostazioni dell'adattatore per solax-x1-mini-rs485",
+        'es': 'Ajustes del adaptador para solax-x1-mini-rs485',
+        'pl': 'Ustawienia adaptera dla solax-x1-mini-rs485',
+        'zh-cn': 'solax-x1-mini-rs485的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
